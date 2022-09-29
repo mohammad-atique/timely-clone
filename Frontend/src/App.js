048bfc7643a58a07088794d7caf925f829c1db02
SonaliBhalerao <sonalibhalerao120@gmail.com>
2022-09-29 21:02:43 +0530

    Profile Page
    profile Page

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import ProfileEditPage from './Components/ProfileEditor-Frontend/ProfileEditPage';
+
 
 function App() {
   return (
     <>
+        <ProfileEditPage />
     </>
   );
 }
