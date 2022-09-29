4564dae77c2d39ad860e4ce8287d16b0c86ce019
rs230498 <singhrobin2304@gmail.com>
2022-09-29 21:16:49 +0530

    day3 robin

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import Tasks from "./components/Task";
 
 function App() {
   return (
-    <div>
-      <h1>Harshal</h1>
-    </div>
+    <>
+      <Tasks/>
+    </>
   );
 }
 
