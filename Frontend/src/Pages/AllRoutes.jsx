d2007e1df3712b4673e20c049fb390b24868d384
rs230498 <singhrobin2304@gmail.com>
2022-09-30 19:12:37 +0530

    day3 robin

--- Frontend/src/Pages/AllRoutes.jsx
+++ Frontend/src/Pages/AllRoutes.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Tasks from "../Components/Task";
 import HomePage from "./HomePage";
 
 const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/tasks" element={<Tasks />} />
     </Routes>
   );
 };
