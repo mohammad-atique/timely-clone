bd1d86875e3406f325e1ddf4539f60b7d74e4751
IntriguedSayan <sayan.mid590@gmail.com>
2022-10-02 13:20:07 +0530

    backend completed

--- Frontend/src/Redux/App-reducer/AppReducer.js
+++ Frontend/src/Redux/App-reducer/AppReducer.js
@@ -1,14 +1,26 @@
 import * as types from "./actionTypes";
 
-const Data = {
-  
+const initData = {
+
+  isLoading:false,
+  isError:false,
+  projectsData:[],
+  name:""
+
 };
 
-export const AppReducer = (state = Data, action) => {
+export const AppReducer = (oldState = initData, action) => {
   const { type, payload } = action;
   switch (type) {
-   
+
+    case types.GET_PROJECTS_REQUEST: return{...oldState,isLoading:true}
+
+    case types.GET_PROJECTS_SUCCESSFULL: return{...oldState,isLoading:false,projectsData:[...payload.data],name:payload.name}
+
+    case types.GET_PROJECTS_FAILURE: return{...oldState,isError:true}
+
     default:
-      return state;
+      return oldState;
+      
   }
 };
