88396cac11b31d4a64eb887364f89a9c3ed14571
harshalpatil655 <harshalpatil655@gmail.com>
2022-10-02 13:12:20 +0530

    addedsomething

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,12 +1,11 @@
 import "./App.css";
-import Sidebar from "./Components/Sidebar";
+
 import AllRoutes from "./Pages/AllRoutes";
 
 function App() {
   return (
     <div>
-      {/* <AllRoutes /> */}
-      <Sidebar />
+      <AllRoutes />
     </div>
   );
 }
